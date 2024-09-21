622e949df63d6d7c24582c4fc68fc12e59d0b8e1
ILUA BARANOV <iluxa182@yandex.ru>
2024-09-21 20:30:50 +0300

    Initial commit

---
+++ src/main/java/com/merion/spring/book/controller/BookApiController.java
@@ -0,0 +1,47 @@
+package com.merion.spring.book.controller;
+
+import com.merion.spring.base.exception.ResourceNotFoundException;
+import com.merion.spring.book.entity.BookEntity;
+import com.merion.spring.book.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BookApiController {
+    private final BookService bookService;
+
+    public BookApiController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/")
+    public String ok(){
+        return "ok";
+    }
+
+    @GetMapping("/api/v1/book")
+    public List<BookEntity> all(){
+        return bookService.all();
+    }
+
+    @GetMapping("/api/v1/book/{id}")
+    public BookEntity byId(@PathVariable Integer id){
+        return bookService.byId(id).orElseThrow(ResourceNotFoundException::new);
+    }
+
+    @PostMapping("/api/v1/book")
+    public BookEntity create(@RequestBody BookEntity request){
+        return bookService.create(request.getTitle(), request.getDescription());
+    }
+
+    @PutMapping("/api/v1/book/{id}")
+    public BookEntity edit(@PathVariable Integer id, @RequestBody BookEntity request){
+        return bookService.edit(request).orElseThrow(ResourceNotFoundException::new);
+    }
+    @DeleteMapping("/api/v1/book/{id}")
+    public Boolean delete (@PathVariable Integer id){
+        return bookService.delete(id);
+
+    }
+}
---
+++ src/main/java/com/merion/spring/book/service/BookService.java
@@ -0,0 +1,73 @@
+package com.merion.spring.book.service;
+
+import com.merion.spring.book.entity.BookEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class BookService {
+    static List<BookEntity> bookStorage = new ArrayList<>();
+
+    public BookService() {
+        fillStorage();
+    }
+
+    public void fillStorage() {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            BookEntity book = new BookEntity();
+            book.setId(i);
+            book.setTitle("Book #" + random.nextInt(100, 999));
+            book.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+            bookStorage.add(book);
+        }
+    }
+
+    public List<BookEntity> all() {
+        return bookStorage;
+    }
+
+    public Optional<BookEntity> byId(Integer id) {
+        return bookStorage.stream().filter((book -> book.getId().equals(id))).findFirst();
+    }
+
+    public BookEntity create(String title, String description) {
+        BookEntity book = new BookEntity();
+        book.setId(bookStorage.size());
+        book.setTitle(title);
+        book.setDescription(description);
+        bookStorage.add(book);
+        return book;
+    }
+
+    public Optional <BookEntity> edit(BookEntity book){
+        Optional<BookEntity> oldbookOptinal = byId(book.getId());
+        if(oldbookOptinal.isEmpty()){
+            return Optional.empty();
+        }
+        BookEntity oldbookEntity = oldbookOptinal.get();
+        oldbookEntity.setTitle(book.getTitle());
+        oldbookEntity.setDescription(book.getDescription());
+        return Optional.of(oldbookEntity);
+
+    }
+
+    public Boolean delete(Integer id){
+        Optional<BookEntity> book = byId(id);
+        if (book.isEmpty()){
+            return false;
+        }
+        bookStorage.remove(book.get());
+
+        return true;
+
+
+    }
+
+
+
+}
